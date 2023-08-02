705ee6f39a0f079a13e72847549cc461bcf78ff9
kamilcakmak <kamilcakmak60@gmail.com>
2023-08-02 06:54:09 +0300

    drf const

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from Restaurant import views
+
+router = DefaultRouter()
+router.register('tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('Restaurant.urls')),
+    path('restaurant/menu/',include('Restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
